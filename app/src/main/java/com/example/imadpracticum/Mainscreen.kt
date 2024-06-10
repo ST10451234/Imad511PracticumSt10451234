3817c40a38d6643ed3b4771205b0e04adfb9b168
Jared <ST10451234@imconnect.edu.za>
2024-06-10 14:41:58 +0200

    2

--- app/src/main/java/com/example/imadpracticum/Mainscreen.kt
+++ app/src/main/java/com/example/imadpracticum/Mainscreen.kt
@@ -15,17 +15,16 @@ class Mainscreen : AppCompatActivity() {
         setContentView(R.layout.activity_mainscreen)
 
         val tempEditText = findViewById<EditText>(R.id.tempEditText)
-        var avrgDisplay = findViewById<TextView>(R.id.avrgdisplay)
+        val avrgDisplay = findViewById<TextView>(R.id.avrgdisplay)
         val detailsButton = findViewById<Button>(R.id.detailsButton)
         val exitbutton =findViewById<Button>(R.id.exitbutton)
         val tempEditTextString = tempEditText.text.toString()
         val tempEditeTextInt = tempEditTextString.toInt()
         val avrgDisplayString = avrgDisplay.text.toString()
-        val avrgDisplayInt = avrgDisplayString.toInt()
-        val temp: Array<Int> = arrayOf(0, 0, 0, 0, 0, 0, 0)
+        avrgDisplayString.toInt()
+        val temp: Array<Int> = arrayOf(7)
         var counter = 1
         var sum = 0
-        var avrg = 0
 
         while (counter <= 7){
             temp[counter] = tempEditeTextInt
@@ -33,10 +32,10 @@ class Mainscreen : AppCompatActivity() {
         }
         var i =0
         while(i<=7){
-            sum = sum + temp[i]
+            sum += temp[i]
             i++
         }
-        avrg = sum/temp.size
+        val avrg: Int = sum/temp.size
         avrgDisplay.text = avrg.toString()
 
 
